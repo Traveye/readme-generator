019c6f87435da02875736297f3d3f75f8557f929
Big Trees <traveye@protonmail.com>
2023-01-21 13:19:08 -0800

    addding user input questions for sections

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Write a description of your project.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Write any usage instructions here.',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Write any installation instructions here.',
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: 'Add any contributors here.',
+      name: 'contribute',
+    },
+    {
+      type: 'input',
+      message: 'Write any test instructions here',
+      name: 'test',
+    },
+    
+  ])
+  .then((response) => {
+    console.log(response)
+  }
+  );
