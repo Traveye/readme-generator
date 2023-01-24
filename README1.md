c2efe2c4f1a37ccf24f7a75e1633ecbcbe70c5a5
Big Trees <traveye@protonmail.com>
2023-01-23 17:57:32 -0800

    adding badges and switch statement

---
+++ README1.md
@@ -0,0 +1,25 @@
+
+  # it is
+    [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+  ## Description
+    it goes
+
+  ## Usage
+    no
+
+  ## Installation
+    maybe
+
+  ## Test
+    sure
+
+  ## Credits
+    ok
+
+  ## License 
+    Apache License 2.0
+
+  ## Questions
+    You can check out this and my other projects at [GitHub](https://github.com/nonon). Please feel free to contact me with any questions at oooo.
+    
--- index.js
+++ index.js
@@ -49,7 +49,7 @@ inquirer
       choices: [
         "MIT",
         "GPL-3.0",
-        "Apache License 2.0:",
+        "Apache License 2.0",
         "GPL-2.0",
         "BSD 3-Clause",
       ],
@@ -57,29 +57,58 @@ inquirer
     },
   ])
   .then((answers) => {
-    console.log(answers);
-    let readme = `
-    # ${answers.title}
+    //ghub link
+    let ghub = `[GitHub](https://github.com/${answers.github})`;
 
-    ## Description
+    //swich case to assign license badge url
+    let badge;
+    let link = answers.license;
+
+    switch (link) {
+      case "MIT":
+        badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+        break;
+      case "GPL-3.0":
+        badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+        break;
+      case "Apache License 2.0":
+        badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+        break;
+      case "GPL-2.0":
+        badge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+        break;
+      case "BSD 3-Clause":
+        badge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+        break;
+    }
+
+    let readme = `
+  # ${answers.title}
+    ${badge}
+
+  ## Description
     ${answers.description}
 
-    ## Usage
+  ## Usage
     ${answers.usage}
 
-    ## Installation
+  ## Installation
     ${answers.install}
 
-    ## Test
+  ## Test
     ${answers.test}
 
-    ## Credits
+  ## Credits
     ${answers.contribute}
 
-    ## License 
+  ## License 
     ${answers.license}
 
-    ## Questions
-    You can check out this and my other projects at ${answers.github}. Please feel free to contact me with any questions at ${email}.
+  ## Questions
+    You can check out this and my other projects at ${ghub}. Please feel free to contact me with any questions at ${answers.email}.
     `;
+
+    fs.writeFile("README1.md", readme, (err) =>
+      err ? console.log(err) : console.log("success")
+    );
   });
